4fc75ff304239f15e1ba3cffafc66472566b8d60
Bleb1k <viktor.masnikov17283940@gmail.com>
2023-02-22 23:28:16 +0900

    Some cleaning + removed GHActions, used JitCI instead

---
+++ src/main/java/com/github/gtgolden/gregtech/GregTech.java
@@ -0,0 +1,10 @@
+package com.github.gtgolden.gregtech;
+
+import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
+import net.modificationstation.stationapi.api.registry.ModID;
+import net.modificationstation.stationapi.api.util.Null;
+
+public class GregTech {
+    @Entrypoint.ModID
+    public static final ModID MOD_ID = Null.get();
+}
